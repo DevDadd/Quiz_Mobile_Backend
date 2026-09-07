84a583a46d6971e5ec75f1021c7e92e8c4a2b689
Ducanh0404 <ducanhjk68@gmail.com>
2026-09-07 20:31:13 +0700

    Fix syntax error in auth urls after refactoring

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,21 +1,10 @@
 from django.urls import path
-from authentication.views import LoginView
+from authentication.views import LoginView, RegisterView, LogoutView, HeartbeatView, MeView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-]
-from authentication.views import RegisterView, LogoutView, HeartbeatView, MeView, OnlinePlayersView
-
-urlpatterns.extend([
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('me/', MeView.as_view(), name='me'),
     path('session/heartbeat/', HeartbeatView.as_view(), name='heartbeat'),
-    path('players/online/', OnlinePlayersView.as_view(), name='players_online'),
-])
-
-from authentication.views import LeaderboardView, UserStatsView, UserMatchesView, QuestionsView, QuestionDetailView
-
-urlpatterns.extend([
-                    ])
-
+]
